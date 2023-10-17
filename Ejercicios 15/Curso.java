38db13dce921a1cb7330a72e32b470136c6fb71a
Karen cortes <kscortesdias@gmail.com>
2023-10-17 17:05:17 -0500

    new

--- Ejercicios 15/Curso.java
+++ Ejercicios 15/Curso.java
@@ -49,11 +49,34 @@ public class Curso {
 
 
     public void imprimir(){
-        System.out.println("|-------------------------------|");
-        System.out.println("|          DETALLE CURSO        |");
-        System.out.println("|-------------------------------|");
-        System.out.println("|                               |");
-        System.out.println("|  Codigo: "+codigo +            "|");
+        System.out.println();
+        System.out.println("|--------------------------------------|");
+        System.out.println("|            DETALLE CURSO             |");
+        System.out.println("|--------------------------------------|");
+        System.out.println("|                                      |");
+        System.out.println("|  Codigo: "+codigo +"                      |");
+        System.out.println("|  Nombre: "+nombre +"                        |");
+        System.out.println("|  Area conocimiento: "+areaConocimiento +"       |");
+        System.out.println("|  Duracion: "+duracionHoras+"                      |");
+        System.out.println("|--------------------------------------|");
+        System.out.println();
+    } 
+
+    public void imprimirListado(Curso[] array){
+        System.out.println();
+        System.out.println("|--------------------------------------|");
+        System.out.println("|           DETALLE LISTADO            |");
+        System.out.println("|--------------------------------------|");
+        System.out.println("|                                      |");
+        System.out.println("|  Codigo      Nombre     Duracion hr  |");
+        System.out.println("|                                      |");
+        for(int i =0; i<array.length; i++){
+            if(array[i]!=null){
+               System.out.println("|  "+array[i].codigo+"       "+array[i].nombre+"         "+array[i].duracionHoras+"      |");
+            }
+        } 
+        System.out.println("|--------------------------------------|");
+        System.out.println();
     }
     
 }
--- Ejercicios 15/Principal.java
+++ Ejercicios 15/Principal.java
@@ -8,19 +8,48 @@ public class Principal{
 
         int size = 99;
         Curso array [] = new Curso [size];
+        int opcion = 0; 
+        Curso ejemplo = new Curso("Ejemplo", "NombreEjemplo", "TecnologiaEjemplo", 100);
+        Curso curso1 = new Curso("224010","ADSO","Tecnologia",1000);
+        Curso curso2 = new Curso("221010","BARI","Tecnologia",2000);
+        array[0]= curso1;
+        array[1]= curso2;
+        do{
+            System.out.println("|--------------------------------|");
+            System.out.println("|          MENU DE CURSOS        |");
+            System.out.println("|--------------------------------|");
+            System.out.println("|                   MEMORIA:"+size+"   |");
+            System.out.println("|  1.Crear curso                 |");
+            System.out.println("|  2.Listar todos los cursos     |");
+            System.out.println("|  3. Ver detalle de curso       |");
+            System.out.println("|  4.Editar informacion de curso |");
+            System.out.println("|  5.Agregar tema al curso       |");
+            System.out.println("|  6.Eliminar curso              |");
+            System.out.println("|  7.Salir                       |");
+            System.out.println("|--------------------------------|");
+            System.out.print("-> Ingrese la opcion que desee (1 a 7): "); 
+            opcion = tecladoN.nextInt(); 
 
+            if(opcion>=1 && opcion<=7){
+                if(opcion == 1){
+        
+                }else if(opcion == 2){
+                    ejemplo.imprimirListado(array);
+                }else if(opcion == 3){
+                    curso1.imprimir();
+                }else if(opcion == 4){
 
-        System.out.println("|--------------------------------|");
-        System.out.println("|          MENU DE CURSOS        |");
-        System.out.println("|--------------------------------|");
-        System.out.println("|                   MEMORIA:"+size+"   |");
-        System.out.println("|  1.Crear curso                 |");
-        System.out.println("|  2.Listar todos los cursos     |");
-        System.out.println("|  3. Ver detalle de curso       |");
-        System.out.println("|  4.Editar informacion de curso |");
-        System.out.println("|  5.Agregar tema al curso       |");
-        System.out.println("|  6.Eliminar curso              |");
-        System.out.println("|  7.Salir                       |");
-        System.out.println("|--------------------------------|");
+                }else if(opcion == 5){
+
+                }else if(opcion == 6){
+
+                }else if(opcion == 7){
+                    System.out.println("-> Ha salido con exito del programa "); 
+                }
+            }else{
+                System.out.println("-> Ingrese una opcion correcta, intentelo nuevamente: "); 
+            }
+        }while(opcion!=7);
+        
     }
 }
