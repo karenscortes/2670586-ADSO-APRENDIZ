d8ee676785da340c51be314fe002460afb9c3627
Karen cortes <kscortesdias@gmail.com>
2023-10-27 16:29:16 -0500

    new

---
+++ Ejercicios 17 - POO IU/Principal.java
@@ -0,0 +1,71 @@
+
+import javax.swing.*; 
+import java.awt.*;
+
+public class Principal{
+
+    public static void main(String[] args){
+
+        JFrame ventana1 = new JFrame();
+        ventana1.setTitle("Primer ventana"); 
+        ventana1.setVisible(true);
+        ventana1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        ventana1.setSize(500,500);
+        ventana1.setSize(new Dimension(300,300));
+        // ventana1.setResizable( false);
+        ventana1.setLocation(500,100);
+        ventana1.setLocationRelativeTo(null);
+
+        JPanel contenedor = new JPanel(); 
+        contenedor.setBackground(Color.PINK); 
+        contenedor.setVisible(true);
+
+        JLabel etiquetaTitulo = new JLabel(); 
+        etiquetaTitulo.setText("TITULO"); 
+        etiquetaTitulo.setForeground(Color.CYAN);
+        etiquetaTitulo.setOpaque(true);
+        etiquetaTitulo.setBackground(Color.WHITE);
+        etiquetaTitulo.setFont( new Font("Snap ITC",Font.BOLD,60));
+
+        JLabel etiquetaCedula = new JLabel(); 
+        etiquetaCedula.setText("Cedula: "); 
+        etiquetaCedula.setFont( new Font("Times new roman",Font.PLAIN,30));
+
+        JTextField cedula = new JTextField(); 
+        cedula.setColumns(10);
+
+        JLabel etiquetaNombre = new JLabel(); 
+        etiquetaNombre.setText("Nombre: "); 
+        etiquetaNombre.setFont( new Font("Times new roman",Font.PLAIN,30));
+
+        JTextField nombre = new JTextField(); 
+        nombre.setColumns(10);
+
+        JLabel etiquetaApellido = new JLabel(); 
+        etiquetaApellido.setText("Cedula: "); 
+        etiquetaApellido.setFont( new Font("Times new roman",Font.PLAIN,30));
+
+        JTextField apellido = new JTextField(); 
+        apellido.setColumns(10);
+
+        JButton boton = new JButton(); 
+        boton.setText("Enviar");
+        boton.setBackground(Color.CYAN);
+
+
+
+        contenedor.add(etiquetaTitulo);
+        contenedor.add(etiquetaCedula);
+        contenedor.add(cedula);
+        contenedor.add(etiquetaNombre); 
+        contenedor.add(nombre);
+        contenedor.add(etiquetaApellido);
+        contenedor.add(apellido);
+        contenedor.add(boton);
+        ventana1.add(contenedor);
+        // ventana1.repaint();
+        ventana1.revalidate(); 
+
+
+    }
+}
